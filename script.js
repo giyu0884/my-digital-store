06b3b5f359fccfcb6ad212a3588e14cf5a02fbc3
giyu0884 <giyu0884@gmail.com>
2025-11-09 14:37:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('buyButton').addEventListener('click', () => {
+  alert('Payment system coming soon!');
+});
+document.getElementById('buyButton').addEventListener('click', () => {
+  alert('Payment system coming soon!');
+});
